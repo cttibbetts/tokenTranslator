2bcc1a2a6db721df194ceb9404a65b00a4d16b38
Tucker Tibbetts <ctibbetts@novetta.com>
2016-07-20 22:08:29 -0400

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # tokenTranslator
 Take in the token file, spit out a translated version
+
+First, get yourself a microsoft account and add http://datamarket.azure.com/dataset/bing/microsofttranslator
+
+Then, in your [account](http://datamarket.azure.com/dataset/bing/microsofttranslator) find your account key and customer id.
+Add them both in app.js:
+```javascript
+	var client = new MsTranslator({
+		client_id: '[your customer id]',
+		client_secret: '[your account key]'
+	}, true)
+```
+
+Change the `toLanguage` variable to change the output language.
+
+put the `tokens.js` file at the root of this directory, and run `node app.js`
+the app will write to `out.txt` in that language. 
+
+
+forgive the terrible readme, this is super alpha of a crappy dev tool only for us
---
+++ app.js
@@ -0,0 +1,90 @@
+var tokenFile;
+define = function(obj) {
+	tokenFile = obj;
+ }
+
+var MsTranslator = require('mstranslator');
+var fs = require('fs');
+require('./tokens.js');
+
+var fromLanguage = 'en';
+var toLanguage = 'ar';
+
+var client = new MsTranslator({
+		client_id: '[your customer id]',
+		client_secret: '[your account key]'
+	}, true)
+
+var paths = [];
+var tokens = [];
+function addTokens(obj, path) {
+	path = path || [];
+	for (var key in obj) {
+		var newPath = path.slice(0);
+		newPath.push(key);
+		if (typeof(obj[key]) === 'string') {
+			paths.push(newPath);
+			tokens.push(obj[key]);
+		} else {
+			addTokens(obj[key], newPath);
+		}
+	}
+}
+
+addTokens(tokenFile);
+
+function addNewToken(token, root, path) {
+	var next = path[0];
+	if (!root[next]) {
+		root[next] = {};
+	}
+
+	path = path.slice(1);
+	if (path.length > 0) {
+		addNewToken(token, root[next], path);
+	} else {
+		root[next] = token;
+	}
+}
+
+var newTokens = {};	
+var chunk = 50;
+var results = 0;
+var failures = 0;
+for (var c = 0, len = tokens.length; c < len; c+=chunk) {
+	var tokenChunk = tokens.slice(c, c+chunk);
+	(function(tokenChunk, chunkIndex) {
+		client.translateArray({
+			texts: tokenChunk,
+			from: fromLanguage,
+			to: toLanguage
+		}, function(err, data) {
+			if (err) {
+				console.log('ERROR:', err);
+				failures++;
+				return;
+			}
+			for (var i in data) {
+				addNewToken(data[i].TranslatedText, newTokens, paths[chunkIndex + +i]);
+				results++;
+			}
+		});
+	})(tokenChunk, c);
+}
+
+intervalId = setInterval(function() {
+	if (results >= tokens.length || results + (failures*50) >= tokens.length) {
+		clearInterval(intervalId);
+		console.log('Got this many results', results, ' --- and this many failures:', failures, ' --- with this many tokens put in:', tokens.length);
+
+		var out = JSON.stringify(newTokens, null, 4);
+		out = '// jscs:disable maximumLineLength\n\ndefine(' + out + ');\n';
+
+		fs.writeFile("out.txt", out, function(err) {
+		    if(err) {
+		        return console.log(err);
+		    }
+		}); 
+	}
+}, 500)
+
